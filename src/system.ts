70fbcdb026be7ab2ba5f39e919998a48ec43dec1
chaosgpt <127708707+chaosgpt@users.noreply.github.com>
2023-03-29 11:54:02 +0000

    bug fix

--- src/system.ts
+++ src/system.ts
@@ -10,12 +10,16 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const message = `
+export const defaultMessage = `MossGPT 
 - [[Shift]] + [[Enter]] 换行。
 
-- 开头输入 [[/]] 或者 [[空格]] Prompt 预设。
+- 开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。
 
 - [[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
+
+export type Setting = typeof defaultSetting
+
+export const defaultResetContinuousDialogue = false
 
 export const defaultMaxInputTokens: Record<Model, number> = {
   "gpt-3.5-turbo": 3072,
